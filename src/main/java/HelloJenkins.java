7bad0b6c5fad593c92042cee13026d2085542e0c
keshikast <keshikathirums@gmail.com>
2026-09-23 10:54:39 +0530

    add maven, change jenkinsfile again default check 1 min dfinal

--- src/main/java/HelloJenkins.java
+++ src/main/java/HelloJenkins.java
@@ -1,7 +1,7 @@
 public class HelloJenkins {
 
     public static String message() {
-        return "Hello from Jenkins! change for 1 minconfirm";
+        return "Hello from Jenkins! change for 1 minconfirm bro cmon";
     }
 
     public static void main(String[] args) {
