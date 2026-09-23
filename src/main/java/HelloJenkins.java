3e72108bf836eb59491fe0390f167c2234b1609e
keshikast <keshikathirums@gmail.com>
2026-09-23 10:48:15 +0530

    add maven, change jenkinsfile again default check

--- src/main/java/HelloJenkins.java
+++ src/main/java/HelloJenkins.java
@@ -1,7 +1,7 @@
 public class HelloJenkins {
 
     public static String message() {
-        return "Hello from Jenkins! change";
+        return "Hello from Jenkins! change for 1 min";
     }
 
     public static void main(String[] args) {
